718a787c2a4019c93b5857f811e497b35dfad4c8
Nhân <hi@imnhan.com>
2024-04-19 21:30:37 +0700

    wire up Quit action with confirmation

--- src/wm2000/main.py
+++ src/wm2000/main.py
@@ -6,11 +6,15 @@ from wm2000.gui.main_window import Ui_MainWindow
 
 
 class MainWindow(qtw.QMainWindow):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, app, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.app = app
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.setup_shortcuts()
+
+        # Actions
+        self.ui.actionQuit.triggered.connect(self.action_quit)
 
     def setup_shortcuts(self):
         # Since Qt6 Designer doesn't support using StandardKey sequences,
@@ -20,11 +24,22 @@ class MainWindow(qtw.QMainWindow):
         self.ui.actionSave_As.setShortcut(qtg.QKeySequence.StandardKey.SaveAs)
         self.ui.actionQuit.setShortcut(qtg.QKeySequence.StandardKey.Quit)
 
+    def action_quit(self):
+        msgbox = qtw.QMessageBox(self)
+        msgbox.setIcon(qtw.QMessageBox.Icon.Warning)
+        msgbox.setWindowTitle("Quitting")
+        msgbox.setText("Are you sure you want to quit?")
+        msgbox.setStandardButtons(qtw.QMessageBox.Ok | qtw.QMessageBox.Cancel)
+        result = msgbox.exec()
+        if result == qtw.QMessageBox.Ok:
+            self.app.quit()
+        self.ui.statusbar.showMessage("Quit cancelled.")
+
 
 def main():
     app = qtw.QApplication(sys.argv)
 
-    window = MainWindow()
+    window = MainWindow(app)
     window.show()
 
     exit(app.exec())
