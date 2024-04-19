5bc27f1a134b6a03b4905b8d0e1af3705e776a49
Nhân <hi@imnhan.com>
2024-04-19 19:52:06 +0700

    use platform-native shortcuts & linux-native icons

--- src/wm2000/main.py
+++ src/wm2000/main.py
@@ -1,4 +1,5 @@
 from PySide6 import QtWidgets as qtw
+from PySide6 import QtGui as qtg
 import sys
 
 from wm2000.gui.main_window import Ui_MainWindow
@@ -9,6 +10,15 @@ class MainWindow(qtw.QMainWindow):
         super().__init__(*args, **kwargs)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.setup_shortcuts()
+
+    def setup_shortcuts(self):
+        # Since Qt6 Designer doesn't support using StandardKey sequences,
+        # let's set them up here.
+        self.ui.actionNew.setShortcut(qtg.QKeySequence.StandardKey.New)
+        self.ui.actionOpen.setShortcut(qtg.QKeySequence.StandardKey.Open)
+        self.ui.actionSave_As.setShortcut(qtg.QKeySequence.StandardKey.SaveAs)
+        self.ui.actionQuit.setShortcut(qtg.QKeySequence.StandardKey.Quit)
 
 
 def main():
