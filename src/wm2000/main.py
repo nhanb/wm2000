38bb82e3cc2d374f9d8fbe811382666bd66ad4d8
Nhân <hi@imnhan.com>
2024-04-19 23:05:59 +0700

    implement New/Open
    For some reason the file picker returns very slowly...

--- src/wm2000/main.py
+++ src/wm2000/main.py
@@ -1,20 +1,36 @@
-from PySide6 import QtWidgets as qtw
-from PySide6 import QtGui as qtg
+from pathlib import Path
 import sys
 
+from PySide6 import QtCore as qtc
+from PySide6 import QtWidgets as qtw
+from PySide6 import QtGui as qtg
+
 from wm2000.gui.main_window import Ui_MainWindow
+from wm2000.persistence import Database
+
+EXTENSION = ".wm2k"
+APP_NAME = "WebMaker2000"
 
 
 class MainWindow(qtw.QMainWindow):
+    signalFileChosen = qtc.Signal(Path)
+
     def __init__(self, app, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.app = app
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.setWindowTitle(APP_NAME)
+
         self.setup_shortcuts()
 
         # Actions
         self.ui.actionQuit.triggered.connect(self.action_quit)
+        self.ui.actionNew.triggered.connect(self.action_new)
+        self.ui.actionOpen.triggered.connect(self.action_open)
+
+        # Custom signals
+        self.signalFileChosen.connect(self.signal_file_chosen)
 
     def setup_shortcuts(self):
         # Since Qt6 Designer doesn't support using StandardKey sequences,
@@ -34,6 +50,48 @@ class MainWindow(qtw.QMainWindow):
         if result == qtw.QMessageBox.Ok:
             self.app.quit()
         self.ui.statusbar.showMessage("Quit cancelled.")
+
+    def action_new(self):
+        file_name, _ = qtw.QFileDialog.getSaveFileName(
+            self,
+            caption="New File",
+            filter=f"WebMaker2000 files (*{EXTENSION})",
+        )
+        if not file_name:
+            return
+
+        if not file_name.endswith(EXTENSION):
+            file_name += EXTENSION
+
+        path = Path(file_name)
+        if path.is_file():
+            path.unlink()
+
+        self.app.db = Database(path, init=True)
+        self.signalFileChosen.emit(path)
+
+    def action_open(self):
+        file_name, _ = qtw.QFileDialog.getOpenFileName(
+            self,
+            caption="Open File",
+            filter=f"WebMaker2000 files (*{EXTENSION})",
+        )
+        if not file_name:
+            return
+
+        path = Path(file_name)
+        self.app.db = Database(path, init=False)
+        self.signalFileChosen.emit(path)
+
+    def signal_file_chosen(self, path: Path):
+        self.setWindowTitle(f"{path} - {APP_NAME}")
+
+        # Enable actions that require a chosen file:
+        self.ui.menuExport.setEnabled(True)
+        self.ui.menuPublish.setEnabled(True)
+        self.ui.actionSave_As.setEnabled(True)
+
+        self.ui.statusbar.showMessage(f"Chosen file: {path}")
 
 
 def main():
---
+++ src/wm2000/persistence/__init__.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+import re
+import sqlite3
+from importlib.resources import files
+
+
+class Database:
+    def __init__(self, path: Path, *, init: bool):
+        self.path: Path = path
+        self.conn = sqlite3.connect(path)
+        self.conn.create_function("REGEXP", 2, regexp)
+        self.conn.row_factory = dict_factory
+        self.conn.execute("PRAGMA foreign_keys = ON;")
+        self.conn.execute("PRAGMA busy_timeout = 4000;")
+
+        if init:
+            self._init_schema()
+
+    def _init_schema(self):
+        sql = files("wm2000.persistence").joinpath("schema.sql").read_text()
+        self.conn.cursor().executescript(sql)
+        return self
+
+
+def regexp(expr, item):
+    reg = re.compile(expr)
+    return reg.search(item) is not None
+
+
+def dict_factory(cursor, row):
+    fields = [column[0] for column in cursor.description]
+    return {key: value for key, value in zip(fields, row)}
